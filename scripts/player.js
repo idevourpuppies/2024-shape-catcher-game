11bb12ac43c958bf986691252c84060c0cce5f33
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-10-01 16:22:43 -0600

    10/1/24

--- scripts/player.js
+++ scripts/player.js
@@ -7,9 +7,95 @@ export class Player {
         this.y=y;
         this.width = 75;
         this.height = 25;
+
+        this.speed = 1;
+
+        this.keyBinding = {
+            up: "ArrowUp",
+            down: "ArrowDown",
+            right: "ArrowRight",
+            left: "Arrowleft",
+        };
+
+        this.moving = {
+            up: false,
+            down: false,
+            right: false,
+            left: false,
+        };
+
+        this.wiredUpKeyBoard();
     }
 
-    update() {}
+wiredUpKeyBoard() {
+    window.addEventListener("keydown", (e) => {
+    // console.log(e);
+    
+    switch(e.code) {
+    case this.keyBinding.up:
+        this.moving.up=true;
+        break;
+        case this.keyBinding.down:
+        this.moving.down=true;
+        break;
+        case this.keyBinding.up:
+        this.moving.right=true;
+        break;
+        case this.keyBinding.left:
+        this.moving.left=true;
+        break;
+    }
+    });
+
+    window.addEventListener("keyup", (e) => {
+    // console.log(e);
+    switch(e.code) {
+        case this.keyBinding.up:
+            this.moving.up=false;
+            break;
+            case this.keyBinding.down:
+            this.moving.down=false;
+            break;
+            case this.keyBinding.up:
+            this.moving.right=false;
+            break;
+            case this.keyBinding.left:
+            this.moving.left=false;
+            break;
+        }
+    });
+}
+    update() {
+        let dirx = 0;
+        let diry = 0;
+
+        if(this.moving.up) {
+            diry = -1;
+        }
+
+        if(this.moving.down) {
+            diry = 1;
+        }
+
+        if(this.moving.left) {
+            dirx = -1;
+        }
+
+        if(this.moving.right) {
+            dirx = 1;
+        }
+
+        if(this.moving.up && this.moving.down) {
+            diry = 0;
+        }
+
+        if(this.moving.left && this.moving.right) {
+            dirx = 0;
+        }
+
+        this.x += this.speed * dirx
+        this.y += this.speed * diry
+    }
 
     draw() {
         ctx.fillStyle = "white";
