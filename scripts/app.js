7d7495713e218796c757958a9388e3793dcbe0c2
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-10-02 22:25:56 -0600

    10/2/24

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,18 @@ import { canvas, ctx } from "./common/canvas.js";
 import { Player } from "./player.js";
 
 let player = new Player();
+
+let lastTimeStamp = 0;
+
+function gameloop(timeSatmp) {
+    let elapsedtime = timeSatmp - lastTimeStamp;
+    lastTimeStamp = timeSatmp;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+player.update();
 player.draw();
+window.requestAnimationFrame(gameloop);
+}
+
+window.requestAnimationFrame(gameloop);
--- scripts/player.js
+++ scripts/player.js
@@ -1,105 +1,104 @@
 //@ts-check
-import { canvas, ctx } from "./common/canvas.js"
+import { canvas, ctx } from "./common/canvas.js";
 
 export class Player {
-    constructor(x = 0,y = 0) {
-        this.x=x;
-        this.y=y;
-        this.width = 75;
-        this.height = 25;
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+    this.width = 75;
+    this.height = 25;
 
-        this.speed = 1;
+    this.speed = 1;
 
-        this.keyBinding = {
-            up: "ArrowUp",
-            down: "ArrowDown",
-            right: "ArrowRight",
-            left: "Arrowleft",
-        };
+    this.keyBinding = {
+      up: "ArrowUp",
+      down: "ArrowDown",
+      right: "ArrowRight",
+      left: "ArrowLeft",
+    };
 
-        this.moving = {
-            up: false,
-            down: false,
-            right: false,
-            left: false,
-        };
+    this.moving = {
+      up: false,
+      down: false,
+      right: false,
+      left: false,
+    };
 
-        this.wiredUpKeyBoard();
-    }
+    this.wiredUpKeyBoard();
+  }
 
-wiredUpKeyBoard() {
+  wiredUpKeyBoard() {
     window.addEventListener("keydown", (e) => {
-    // console.log(e);
-    
-    switch(e.code) {
-    case this.keyBinding.up:
-        this.moving.up=true;
-        break;
-        case this.keyBinding.down:
-        this.moving.down=true;
-        break;
+      // console.log(e);
+
+      switch (e.code) {
         case this.keyBinding.up:
-        this.moving.right=true;
-        break;
+          this.moving.up = true;
+          break;
+        case this.keyBinding.down:
+          this.moving.down = true;
+          break;
+        case this.keyBinding.right:
+          this.moving.right = true;
+          break;
         case this.keyBinding.left:
-        this.moving.left=true;
-        break;
-    }
+          this.moving.left = true;
+          break;
+      }
     });
 
     window.addEventListener("keyup", (e) => {
-    // console.log(e);
-    switch(e.code) {
+      // console.log(e);
+      switch (e.code) {
         case this.keyBinding.up:
-            this.moving.up=false;
-            break;
-            case this.keyBinding.down:
-            this.moving.down=false;
-            break;
-            case this.keyBinding.up:
-            this.moving.right=false;
-            break;
-            case this.keyBinding.left:
-            this.moving.left=false;
-            break;
-        }
+          this.moving.up = false;
+          break;
+        case this.keyBinding.down:
+          this.moving.down = false;
+          break;
+        case this.keyBinding.right:
+          this.moving.right = false;
+          break;
+        case this.keyBinding.left:
+          this.moving.left = false;
+          break;
+      }
     });
-}
-    update() {
-        let dirx = 0;
-        let diry = 0;
+  }
+  update() {
+    let dirx = 0;
+    let diry = 0;
 
-        if(this.moving.up) {
-            diry = -1;
-        }
-
-        if(this.moving.down) {
-            diry = 1;
-        }
-
-        if(this.moving.left) {
-            dirx = -1;
-        }
-
-        if(this.moving.right) {
-            dirx = 1;
-        }
-
-        if(this.moving.up && this.moving.down) {
-            diry = 0;
-        }
-
-        if(this.moving.left && this.moving.right) {
-            dirx = 0;
-        }
-
-        this.x += this.speed * dirx
-        this.y += this.speed * diry
+    if (this.moving.up) {
+      diry = -1;
     }
 
-    draw() {
-        ctx.fillStyle = "white";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+    if (this.moving.down) {
+      diry = 1;
     }
 
+    if (this.moving.left) {
+      dirx = -1;
+    }
+
+    if (this.moving.right) {
+      dirx = 1;
+    }
+
+    if (this.moving.up && this.moving.down) {
+      diry = 0;
+    }
+
+    if (this.moving.left && this.moving.right) {
+      dirx = 0;
+    }
+
+    this.x += this.speed * dirx;
+    this.y += this.speed * diry;
+  }
+
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 }
